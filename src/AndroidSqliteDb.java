3657b2aef41935056629a64dcd1bc3bb6074cabb
maderkse <maderkse@gmail.com>
2017-02-12 22:49:39 +0100

    v0.3 s almost working

--- src/AndroidSqliteDb.java
+++ src/AndroidSqliteDb.java
@@ -14,13 +14,13 @@ public class AndroidSqliteDb
 	private static String cQuery = "select sqlite_version() AS sqlite_version";
 	private static Cursor cCursor = null;	
 	private static int cColCount = 0;
-	
+
 	public static void main(String[] args)
 	{   
 		System.out.println("Database: " + cDbName);
 		System.out.println("Db type: Sqlite");
 		System.out.print("Db version: ");
-		
+
 	    cCursor = cDb.rawQuery(cQuery, null);
 		while (cCursor.moveToNext())
 		{
@@ -37,64 +37,102 @@ public class AndroidSqliteDb
 		}
 		cCursor.close();
 
-		
+
 
 		Scanner input = new Scanner(System.in);
+
+		outerloop :
         while (true)
-		{
-			String lPrevQuery = cQuery; 
+		{		
+			
+			String lPrevQuery = cQuery;
 			System.out.print("$: ");
-			cQuery = input.nextLine();
-			
-			if (cQuery.isEmpty()) break;
 			input.reset();
-			
-			switch (cQuery) {
+			cQuery = input.nextLine();
+					
+			if (cQuery.isEmpty()) break;
+
+			switch (cQuery)
+			{
 				case "s" : {
-				   save(lPrevQuery);
-				   break;
-				}
-				default : execute(cQuery);
-			}
+						save(lPrevQuery);
+						break;
+					}				
+				case "q" : break outerloop;					
+				default : {
+						if (cQuery.matches("^[0-9]+$"))
+						{   String lQuery = "select query from queries where id=" + cQuery;
+							Cursor lCursor = execute(lQuery);
+							if (!lCursor.moveToNext()) continue;
+							
+							cQuery = lCursor.getString(0);
+						}
 						
+						showResult(execute(cQuery));
+							
+						}
+			}
+
+		}
+		
+		if (!cCursor.isClosed())
+		{
+			cCursor.close();
 		}
 		
 		cDb.close();
 	}
-	
-	public static void execute(String pRawQuery) {
-		try {
-			cCursor = cDb.rawQuery(pRawQuery, null);
-			cColCount = cCursor.getColumnCount();
-			int i=0;
-			String lKeyword = cQuery.length() > 6 ? cQuery.substring(0,6) : "";
 
-			if ( lKeyword.equals("select")) { 
-
-				for (i = 0;i < cColCount - 1;i++) 
-					System.out.print("'" + padRight(cCursor.getColumnName(i), 10) + "'");	
-				System.out.println("'" + padRight(cCursor.getColumnName(i), 10) + "'");
-			}
-			while (cCursor.moveToNext())
-			{			
-				for (i = 0;i < cColCount - 1;i++) 
-					System.out.print("'" + padRight(cCursor.getString(i), 10) + "'");
-				System.out.println("'" + padRight(cCursor.getString(i), 10) + "'");
-			}
-		} catch (Exception e){
-			System.out.println(e);
-		} finally {
-			cCursor.close();
+	public static Cursor execute(String pRawQuery)
+	{   Cursor lCursor = null;
+		try
+		{
+			lCursor = cDb.rawQuery(pRawQuery, null);
+			
 		}
+		catch (Exception e)
+		{
+			lCursor = null;
+			System.out.println(e);
+		}
+		
+		
+		return lCursor;
 	}
+	
+	public static void showResult(Cursor pCursor) {
+		int lColCount = pCursor.getColumnCount();
+		int i=0;
+		String lKeyword = cQuery.length() > 6 ? cQuery.substring(0, 6) : "";
+
+		if (pCursor == null) return;
+		
+		if (lKeyword.equals("select"))
+		{ 
+
+			for (i = 0;i < lColCount - 1;i++) 
+				System.out.print("'" + padRight(pCursor.getColumnName(i), 10) + "'");	
+			System.out.println("'" + padRight(pCursor.getColumnName(i), 10) + "'");
+		}
+		while (pCursor.moveToNext())
+		{			
+			for (i = 0;i < lColCount - 1;i++) 
+				System.out.print("'" + padRight(pCursor.getString(i), 10) + "'");
+			System.out.println("'" + padRight(pCursor.getString(i), 10) + "'");
+		}
+	}	
+	
 
 	public static String padRight(String s, int n)
 	{
 		return String.format("%1$-" + n + "s", s);  
 	}
-	
-	public static void save(String pQuery) {
+
+	public static void save(String pQuery)
+	{
 		String lDml = "insert into queries (query) values (\"" + pQuery + "\")";
 		execute(lDml);
 	}
+
+
 }
